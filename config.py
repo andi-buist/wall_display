ffc9d4f3735fccb41b6d05e4d63f2fb43fe89f5f
Andrew Buist <105498051+andrew-buist@users.noreply.github.com>
2026-01-13 18:23:43 +0000

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-hass_config = {
-    "url": "ws://192.168.0.180:8123/api/websocket",
-    "secret": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxZTg1MmJjMDlmYjE0NzFkYjJkMjMyOGIyYmY0YmNmMyIsImlhdCI6MTc1Nzk3MDUzNiwiZXhwIjoyMDczMzMwNTM2fQ.6d23es6uAiDb4jloOVofhelRgk-EZT95vyWzhO68ic4"
-}
-
-astronomy_config = {
-    "id": "fcdff912-bb48-45f5-8ec8-4be94a883e37",
-    "secret": "d38f3a1bd0ac83f65b176cc95d48c253170270282efbda52cca553d9758a971864ee4be6e667c2a0dcd46c9dc276d8a9d024924b62def386cca8a2c7251136d69cefead4dda193b11b22dfa7e5073102b48d5339d6b70b475c2fe62930f429e1e5467d0eeb72a493fad43793949e1498"
-}
